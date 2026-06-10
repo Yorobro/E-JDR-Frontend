df0c1df27ab2d91c7f487cb122dbbb5ceea57b55
Finelle Yohan <104372934+Yorobro@users.noreply.github.com>
2026-06-11 00:44:54 +0200

    feat(auth): add GetCurrentUserUseCase wired through Koin

---
+++ src/test/kotlin/eu/ejdr/application/auth/usecase/GetCurrentUserUseCaseImplTest.kt
@@ -0,0 +1,38 @@
+package eu.ejdr.application.auth.usecase
+
+import eu.ejdr.application.auth.abstraction.repository.AuthRepository
+import eu.ejdr.application.common.Result
+import eu.ejdr.domain.entities.auth.User
+import eu.ejdr.domain.error.entities.auth.AuthError
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class GetCurrentUserUseCaseImplTest {
+
+    private val repository = mockk<AuthRepository>()
+    private val useCase = GetCurrentUserUseCaseImpl(repository)
+
+    @Test
+    fun `returns user on success`() = runTest {
+        coEvery { repository.me() } returns Result.Success(User("1", "a@b.c"))
+
+        val result = useCase()
+
+        assertIs<Result.Success<User>>(result)
+        assertEquals("a@b.c", result.value.email)
+    }
+
+    @Test
+    fun `propagates SessionExpired failure`() = runTest {
+        coEvery { repository.me() } returns Result.Failure(AuthError.SessionExpired)
+
+        val result = useCase()
+
+        assertIs<Result.Failure<AuthError>>(result)
+        assertEquals(AuthError.SessionExpired, result.error)
+    }
+}
